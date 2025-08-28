5c6a8e8c248de9d438af99138fd0d1e9487b3314
Yashpal Gawali <gawaliyashpal@gmail.com>
2025-08-28 17:21:46 +0530

    first save

---
+++ src/main/java/com/example/demo/ExampleForJenkisApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleForJenkisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleForJenkisApplication.class, args);
+		
+		System.err.println("This is the Jenkins Example");
+	}
+
+}
